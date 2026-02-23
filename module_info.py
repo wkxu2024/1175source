58cb9d20f16507dc50abca1e9238476d168bfd35
wkxu2024 <1147426328@qq.com>
2026-02-23 16:14:30 +0800

    和镇长对话直接显示利润

--- module_info.py
+++ module_info.py
@@ -2,4 +2,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "D:/Steam/steamapps/common/MountBlade Warband/Modules/Native1175/"
+# export_dir = "D:/Steam/steamapps/common/MountBlade Warband/Modules/Native1175/"
+export_dir = "D:/Codes/1175source/bin/"
